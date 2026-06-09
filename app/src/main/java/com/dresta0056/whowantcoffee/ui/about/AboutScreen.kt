7ae6e63e808582313326fe8279137aa7cc970678
nurmareko <nurmareko@yandex.ru>
2026-06-09 13:50:10 +0700

    Make about screen scrollable

--- app/src/main/java/com/dresta0056/whowantcoffee/ui/about/AboutScreen.kt
+++ app/src/main/java/com/dresta0056/whowantcoffee/ui/about/AboutScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -81,6 +83,7 @@ fun AboutContent(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(24.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
